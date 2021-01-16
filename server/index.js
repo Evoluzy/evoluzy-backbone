d59b8614d873f9a44e69bbecb7c19e60bfd08052
hacktron95 <hacktron95@gmail.com>
2021-01-16 19:45:10 +0800

    init firebase, use process.env file for secured access to firebase config keys

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const firebase = require("firebase");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // system wide configs
-// const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
 const port = 3000;
 const app = express();
+
+// system inits and hooks
 app.use(bodyParser.json());
+
 // firebase.initializeApp(firebaseConfig);
+
 // let db = firebase.firestore();
 
 app.get("/", (req, res) => {
